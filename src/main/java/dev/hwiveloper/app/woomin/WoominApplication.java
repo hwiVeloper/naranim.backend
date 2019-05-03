19432a200025fd9173ef982e8fd1084da0d609b5
Jonghwi Lee <zziller03@gmail.com>
2019-05-03 16:50:34 +0900

    spring 환경 세팅

---
+++ src/main/java/dev/hwiveloper/app/woomin/WoominApplication.java
@@ -0,0 +1,13 @@
+package dev.hwiveloper.app.woomin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoominApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoominApplication.class, args);
+	}
+
+}
