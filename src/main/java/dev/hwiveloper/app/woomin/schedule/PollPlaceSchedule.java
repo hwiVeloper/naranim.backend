ae983e7461031cb714f9d363d0372d2c459ac967
Jonghwi Lee <zziller03@gmail.com>
2019-06-05 02:18:04 +0900

    Update PollPlaceSchedule.java

--- src/main/java/dev/hwiveloper/app/woomin/schedule/PollPlaceSchedule.java
+++ src/main/java/dev/hwiveloper/app/woomin/schedule/PollPlaceSchedule.java
@@ -32,122 +32,216 @@ import java.util.List;
 @Component
 public class PollPlaceSchedule {
 
-    @Value("${keys.poll-place-service}")
-    String POLL_PLACE_SERVICE;
+	@Value("${keys.poll-place-service}")
+	String POLL_PLACE_SERVICE;
 
-    @Autowired
-    PolPlaceRepository ppRepo;
+	@Autowired
+	PolPlaceRepository ppRepo;
 
-    @Autowired
-    SungeoguRepository sggRepo;
+	@Autowired
+	SungeoguRepository sggRepo;
 
-    /**
-     * getPrePolplcOtlnmapTrnsportInfoInqire
-     * 사전투표소 정보
-     */
-    @Scheduled(cron="0 0 1 * * *")
-    public void getPrePolplcOtlnmapTrnsportInfoInqire() {
-        try {
-            // sgId, sdName 추출
-            List<Sungeogu> sggList = sggRepo.findDistinctSgIdSdNameBySdNameOrderBySgIdSOrder();
+	/**
+	 * getPrePolplcOtlnmapTrnsportInfoInqire
+	 * 사전투표소 정보
+	 */
+	@Scheduled(cron="0 0 1 * * *")
+	public void getPrePolplcOtlnmapTrnsportInfoInqire() {
+		try {
+			// sgId, sdName 추출
+			List<Sungeogu> sggList = sggRepo.findDistinctSgIdSdNameBySdNameOrderBySgIdSOrder();
 
-            // 본서비스 호출 (loop)
-            for (Sungeogu sgg : sggList) {
-                // URL 생성
-                StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/9710000/NationalAssemblyInfoService/getLocalSearch"); /*URL*/
-                urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + POLL_PLACE_SERVICE); /*Service Key*/
-                urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("9999", "UTF-8")); /*한 페이지 결과 수*/
-                urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
-                urlBuilder.append("&" + URLEncoder.encode("sgId","UTF-8") + "=" + URLEncoder.encode(sgg.getKey().getSgId().toString(), "UTF-8"));
-                urlBuilder.append("&" + URLEncoder.encode("sdName","UTF-8") + "=" + URLEncoder.encode(sgg.getSdName(), "UTF-8"));
-                URL url = new URL(urlBuilder.toString());
+			// 본서비스 호출 (loop)
+			for (Sungeogu sgg : sggList) {
+				// URL 생성
+				StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/9760000/PolplcInfoInqireService2/getPrePolplcOtlnmapTrnsportInfoInqire"); /*URL*/
+				urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + POLL_PLACE_SERVICE); /*Service Key*/
+				urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("9999", "UTF-8")); /*한 페이지 결과 수*/
+				urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+				urlBuilder.append("&" + URLEncoder.encode("sgId","UTF-8") + "=" + URLEncoder.encode(sgg.getKey().getSgId().toString(), "UTF-8"));
+				urlBuilder.append("&" + URLEncoder.encode("sdName","UTF-8") + "=" + URLEncoder.encode(sgg.getSdName(), "UTF-8"));
+				URL url = new URL(urlBuilder.toString());
 
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("Content-type", "application/json");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Content-type", "application/json");
 
-                BufferedReader rd;
-                if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-                    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                } else {
-                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-                }
+				BufferedReader rd;
+				if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+					rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				} else {
+					rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+				}
 
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = rd.readLine()) != null) {
-                    sb.append(line);
-                }
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = rd.readLine()) != null) {
+					sb.append(line);
+				}
 
-                rd.close();
-                conn.disconnect();
+				rd.close();
+				conn.disconnect();
 
-                // 후처리
-                JSONObject tmpJson = XML.toJSONObject(sb.toString())
-                                        .getJSONObject("response")
-                                        .getJSONObject("body")
-                                        .getJSONObject("items");
-                List<PolPlace> listPolPlace = new ArrayList<PolPlace>();
+				// 후처리
+				JSONObject tmpJson = XML.toJSONObject(sb.toString())
+						.getJSONObject("response")
+						.getJSONObject("body")
+						.getJSONObject("items");
+				List<PolPlace> listPolPlace = new ArrayList<PolPlace>();
 
-                // JSONArray | JSONObject 케이스에 따라 분기처리
-                if (tmpJson.get("item") instanceof JSONArray) {
-                    JSONArray itemJson = tmpJson.getJSONArray("item");
-                    for (int i = 0; i < itemJson.length(); i++) {
-                        JSONObject item = itemJson.getJSONObject(i);
-                        PolPlace itemPp = new PolPlace();
-                        PolPlacePK keyPp = new PolPlacePK();
+				// JSONArray | JSONObject 케이스에 따라 분기처리
+				if (tmpJson.get("item") instanceof JSONArray) {
+					JSONArray itemJson = tmpJson.getJSONArray("item");
+					for (int i = 0; i < itemJson.length(); i++) {
+						JSONObject item = itemJson.getJSONObject(i);
+						PolPlace itemPp = new PolPlace();
+						PolPlacePK keyPp = new PolPlacePK();
 
-                        keyPp.setPreYn(true); // Y, N
-                        keyPp.setNum(item.getInt("num"));
-                        keyPp.setSgId(item.get("sgId").toString());
-                        keyPp.setSdName(item.getString("sdName"));
+						keyPp.setPreYn(true); // Y, N
+						keyPp.setNum(item.getInt("num"));
+						keyPp.setSgId(item.get("sgId").toString());
+						keyPp.setSdName(item.getString("sdName"));
 
-                        itemPp.setKey(keyPp);
-                        itemPp.setEvPsName(item.getString("evPsName"));
-                        itemPp.setWiwName(item.getString("wiwName"));
-                        itemPp.setEmdName(item.getString("emdName"));
-                        itemPp.setEvOrder(item.getInt("evOrder"));
-                        itemPp.setPlaceName(item.getString("placeName"));
-                        itemPp.setAddr(item.getString("addr"));
-                        itemPp.setFloor(item.getString("floor"));
+						itemPp.setKey(keyPp);
+						itemPp.setEvPsName(item.getString("evPsName"));
+						itemPp.setWiwName(item.getString("wiwName"));
+						itemPp.setEmdName(item.getString("emdName"));
+						itemPp.setEvOrder(item.getInt("evOrder"));
+						itemPp.setPlaceName(item.getString("placeName"));
+						itemPp.setAddr(item.getString("addr"));
+						itemPp.setFloor(item.getString("floor"));
 
-                        listPolPlace.add(itemPp);
-                    }
-                } else
-                if (tmpJson.get("item") instanceof JSONObject) {
-                    JSONObject itemJson = tmpJson.getJSONObject("item");
-                    PolPlace itemPp = new PolPlace();
-                    PolPlacePK keyPp = new PolPlacePK();
+						listPolPlace.add(itemPp);
+					}
+				} else
+				if (tmpJson.get("item") instanceof JSONObject) {
+					JSONObject itemJson = tmpJson.getJSONObject("item");
+					PolPlace itemPp = new PolPlace();
+					PolPlacePK keyPp = new PolPlacePK();
 
-                    keyPp.setPreYn(true); // Y, N
-                    keyPp.setNum(itemJson.getInt("num"));
-                    keyPp.setSgId(itemJson.get("sgId").toString());
-                    keyPp.setSdName(itemJson.getString("sdName"));
+					keyPp.setPreYn(true); // Y, N
+					keyPp.setNum(itemJson.getInt("num"));
+					keyPp.setSgId(itemJson.get("sgId").toString());
+					keyPp.setSdName(itemJson.getString("sdName"));
 
-                    itemPp.setKey(keyPp);
-                    itemPp.setEvPsName(itemJson.getString("evPsName"));
-                    itemPp.setWiwName(itemJson.getString("wiwName"));
-                    itemPp.setEmdName(itemJson.getString("emdName"));
-                    itemPp.setEvOrder(itemJson.getInt("evOrder"));
-                    itemPp.setPlaceName(itemJson.getString("placeName"));
-                    itemPp.setAddr(itemJson.getString("addr"));
-                    itemPp.setFloor(itemJson.getString("floor"));
+					itemPp.setKey(keyPp);
+					itemPp.setEvPsName(itemJson.getString("evPsName"));
+					itemPp.setWiwName(itemJson.getString("wiwName"));
+					itemPp.setEmdName(itemJson.getString("emdName"));
+					itemPp.setEvOrder(itemJson.getInt("evOrder"));
+					itemPp.setPlaceName(itemJson.getString("placeName"));
+					itemPp.setAddr(itemJson.getString("addr"));
+					itemPp.setFloor(itemJson.getString("floor"));
 
-                    listPolPlace.add(itemPp);
-                }
+					listPolPlace.add(itemPp);
+				}
 
-                ppRepo.saveAll(listPolPlace);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+				ppRepo.saveAll(listPolPlace);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * getPolplcOtlnmapTrnsportInfoInqire
-     * 선거일 투표소 정보 조회
-     */@Scheduled(cron="0 30 1 * * *")
-    public void getPolplcOtlnmapTrnsportInfoInqire() {
+	/**
+	 * getPolplcOtlnmapTrnsportInfoInqire
+	 * 선거일 투표소 정보 조회
+	 */
+	@Scheduled(cron="0 30 1 * * *")
+	public void getPolplcOtlnmapTrnsportInfoInqire() {
+		try {
+			// sgId, sdName 추출
+			List<Sungeogu> sggList = sggRepo.findDistinctSgIdSdNameBySdNameOrderBySgIdSOrder();
 
-    }
+			// 본서비스 호출 (loop)
+			for (Sungeogu sgg : sggList) {
+				// URL 생성
+				StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/9760000/PolplcInfoInqireService2/getPolplcOtlnmapTrnsportInfoInqire"); /*URL*/
+				urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + POLL_PLACE_SERVICE); /*Service Key*/
+				urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("9999", "UTF-8")); /*한 페이지 결과 수*/
+				urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+				urlBuilder.append("&" + URLEncoder.encode("sgId","UTF-8") + "=" + URLEncoder.encode(sgg.getKey().getSgId().toString(), "UTF-8"));
+				urlBuilder.append("&" + URLEncoder.encode("sdName","UTF-8") + "=" + URLEncoder.encode(sgg.getSdName(), "UTF-8"));
+				URL url = new URL(urlBuilder.toString());
+
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Content-type", "application/json");
+
+				BufferedReader rd;
+				if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+					rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				} else {
+					rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+				}
+
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = rd.readLine()) != null) {
+					sb.append(line);
+				}
+
+				rd.close();
+				conn.disconnect();
+
+				// 후처리
+				JSONObject tmpJson = XML.toJSONObject(sb.toString())
+						.getJSONObject("response")
+						.getJSONObject("body")
+						.getJSONObject("items");
+				List<PolPlace> listPolPlace = new ArrayList<PolPlace>();
+
+				// JSONArray | JSONObject 케이스에 따라 분기처리
+				if (tmpJson.get("item") instanceof JSONArray) {
+					JSONArray itemJson = tmpJson.getJSONArray("item");
+					for (int i = 0; i < itemJson.length(); i++) {
+						JSONObject item = itemJson.getJSONObject(i);
+						PolPlace itemPp = new PolPlace();
+						PolPlacePK keyPp = new PolPlacePK();
+
+						keyPp.setPreYn(true); // Y, N
+						keyPp.setNum(item.getInt("num"));
+						keyPp.setSgId(item.get("sgId").toString());
+						keyPp.setSdName(item.getString("sdName"));
+
+						itemPp.setKey(keyPp);
+						itemPp.setEvPsName(item.getString("evPsName"));
+						itemPp.setWiwName(item.getString("wiwName"));
+						itemPp.setEmdName(item.getString("emdName"));
+						itemPp.setEvOrder(item.getInt("evOrder"));
+						itemPp.setPlaceName(item.getString("placeName"));
+						itemPp.setAddr(item.getString("addr"));
+						itemPp.setFloor(item.getString("floor"));
+
+						listPolPlace.add(itemPp);
+					}
+				} else
+				if (tmpJson.get("item") instanceof JSONObject) {
+					JSONObject itemJson = tmpJson.getJSONObject("item");
+					PolPlace itemPp = new PolPlace();
+					PolPlacePK keyPp = new PolPlacePK();
+
+					keyPp.setPreYn(true); // Y, N
+					keyPp.setNum(itemJson.getInt("num"));
+					keyPp.setSgId(itemJson.get("sgId").toString());
+					keyPp.setSdName(itemJson.getString("sdName"));
+
+					itemPp.setKey(keyPp);
+					itemPp.setEvPsName(itemJson.getString("evPsName"));
+					itemPp.setWiwName(itemJson.getString("wiwName"));
+					itemPp.setEmdName(itemJson.getString("emdName"));
+					itemPp.setEvOrder(itemJson.getInt("evOrder"));
+					itemPp.setPlaceName(itemJson.getString("placeName"));
+					itemPp.setAddr(itemJson.getString("addr"));
+					itemPp.setFloor(itemJson.getString("floor"));
+
+					listPolPlace.add(itemPp);
+				}
+
+				ppRepo.saveAll(listPolPlace);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
