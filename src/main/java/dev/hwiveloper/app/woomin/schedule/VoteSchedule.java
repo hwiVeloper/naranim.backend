aed6f8222c6aa3131b994badbf1accbe2a23563b
Jonghwi Lee <zziller03@gmail.com>
2019-06-09 19:37:21 +0900

    20190609 중간저장

--- src/main/java/dev/hwiveloper/app/woomin/schedule/VoteSchedule.java
+++ src/main/java/dev/hwiveloper/app/woomin/schedule/VoteSchedule.java
@@ -14,15 +14,19 @@ import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import dev.hwiveloper.app.woomin.domain.common.Election;
-import dev.hwiveloper.app.woomin.domain.election.Candidate;
-import dev.hwiveloper.app.woomin.domain.election.CandidatePK;
+import dev.hwiveloper.app.woomin.domain.common.Sungeogu;
 import dev.hwiveloper.app.woomin.domain.election.Vote;
 import dev.hwiveloper.app.woomin.domain.election.VotePK;
+import dev.hwiveloper.app.woomin.domain.election.VoteResult;
+import dev.hwiveloper.app.woomin.domain.election.VoteResultPK;
 import dev.hwiveloper.app.woomin.repository.ElectionRepository;
+import dev.hwiveloper.app.woomin.repository.SungeoguRepository;
 import dev.hwiveloper.app.woomin.repository.VoteRepository;
+import dev.hwiveloper.app.woomin.repository.VoteResultRepository;
 
 /**
  * VoteSchedule
@@ -34,13 +38,19 @@ import dev.hwiveloper.app.woomin.repository.VoteRepository;
 public class VoteSchedule {
 	@Value("${keys.vote-service}")
 	String VOTE_SERVICE;
-	
+
 	@Autowired
 	VoteRepository voteRepo;
 	
 	@Autowired
+	VoteResultRepository voteResultRepo;
+
+	@Autowired
 	ElectionRepository electionRepo;
 	
+	@Autowired
+	SungeoguRepository sggRepo;
+
 	/**
 	 * getVoteSttusInfoInqire
 	 * 투표 결과 조회
@@ -48,7 +58,7 @@ public class VoteSchedule {
 	public void getVoteSttusInfoInqire() {
 		try {
 			List<Election> electionList = (List<Election>) electionRepo.findAll();
-			
+
 			for (Election election : electionList) {
 				// URL 생성
 				StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/9760000/VoteXmntckInfoInqireService2/getVoteSttusInfoInqire"); /*URL*/
@@ -78,15 +88,18 @@ public class VoteSchedule {
 
 				rd.close();
 				conn.disconnect();
-				
+
 				// 후처리
-				// 후처리
-				JSONObject tmpJson = XML.toJSONObject(sb.toString())
+				JSONObject resultBody = XML.toJSONObject(sb.toString())
 						.getJSONObject("response")
-						.getJSONObject("body")
-						.getJSONObject("items");
+						.getJSONObject("body");
+				if (resultBody.getInt("totalCount") == 0) {
+					continue;
+				}
+				JSONObject tmpJson = resultBody.getJSONObject("items");
+
 				List<Vote> listVote = new ArrayList<Vote>();
-				
+
 				// JSONArray | JSONObject 케이스에 따라 분기처리
 				if (tmpJson.get("item") instanceof JSONArray) {
 					JSONArray itemJson = tmpJson.getJSONArray("item");
@@ -108,9 +121,9 @@ public class VoteSchedule {
 						itemVote.setTotTusu(item.getBigDecimal("totTusu"));
 						itemVote.setPsTusu(item.getBigDecimal("psTusu"));
 						itemVote.setPsEtcTusu(item.getBigDecimal("psEtcTusu"));
-						itemVote.setTurnout(item.getBigDecimal("turnou"));
+						itemVote.setTurnout(item.getBigDecimal("turnout"));
 						itemVote.setVrOrder(item.getInt("vrOrder"));
-						
+
 						listVote.add(itemVote);
 					}
 				} else
@@ -132,9 +145,9 @@ public class VoteSchedule {
 					itemVote.setTotTusu(item.getBigDecimal("totTusu"));
 					itemVote.setPsTusu(item.getBigDecimal("psTusu"));
 					itemVote.setPsEtcTusu(item.getBigDecimal("psEtcTusu"));
-					itemVote.setTurnout(item.getBigDecimal("turnou"));
+					itemVote.setTurnout(item.getBigDecimal("turnout"));
 					itemVote.setVrOrder(item.getInt("vrOrder"));
-					
+
 					listVote.add(itemVote);
 				}
 
@@ -144,12 +157,147 @@ public class VoteSchedule {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * getXmntckSttusInfoInqire
 	 * 개표 결과 조회
 	 */
+	@Scheduled(cron="0 */1 * * * *")
 	public void getXmntckSttusInfoInqire() {
-		
+		System.out.println("스케쥴러 시작 :: 개표 결과 조회");
+		try {
+			List<Election> electionList = (List<Election>) electionRepo.findAll();
+
+			//for (Election election : electionList) {
+				// URL 생성
+				StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/9760000/VoteXmntckInfoInqireService2/getXmntckSttusInfoInqire"); /*URL*/
+				urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + VOTE_SERVICE); /*Service Key*/
+				urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("9999", "UTF-8")); /*한 페이지 결과 수*/
+				urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+//				urlBuilder.append("&" + URLEncoder.encode("sgId","UTF-8") + "=" + URLEncoder.encode(election.getKey().getSgId().toString(), "UTF-8"));
+//				urlBuilder.append("&" + URLEncoder.encode("sgTypecode","UTF-8") + "=" + URLEncoder.encode(election.getKey().getSgTypeCode().toString(), "UTF-8"));
+				urlBuilder.append("&" + URLEncoder.encode("sgId","UTF-8") + "=" + URLEncoder.encode("20160413", "UTF-8"));
+				urlBuilder.append("&" + URLEncoder.encode("sgTypecode","UTF-8") + "=" + URLEncoder.encode("4", "UTF-8"));
+				
+				URL url = new URL(urlBuilder.toString());
+
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Content-type", "application/json");
+
+				BufferedReader rd;
+				if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+					rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				} else {
+					rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+				}
+
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = rd.readLine()) != null) {
+					sb.append(line);
+				}
+
+				rd.close();
+				conn.disconnect();
+
+				// 후처리
+				JSONObject resultBody = XML.toJSONObject(sb.toString())
+						.getJSONObject("response")
+						.getJSONObject("body");
+				if (resultBody.getInt("totalCount") == 0) {
+					//continue;
+					return;
+				}
+				JSONObject tmpJson = resultBody.getJSONObject("items");
+
+				List<VoteResult> listVoteResult = new ArrayList<VoteResult>();
+
+				// JSONArray | JSONObject 케이스에 따라 분기처리
+				if (tmpJson.get("item") instanceof JSONArray) {
+					JSONArray itemJson = tmpJson.getJSONArray("item");
+					for (int i = 0; i < itemJson.length(); i++) {
+						JSONObject item = itemJson.getJSONObject(i);
+						
+						// 데이터 정합성 체크
+						if (item.getString("sdName").equals("합계")
+						||  item.getString("wiwName").equals("합계")) {
+							continue;
+						}
+												
+						// 후보자 seq 추출
+						for (int seq = 1; seq <= 30; seq++) {
+							String key = "jd".concat(padLeft(seq, 2));
+							if (item.getString(key).isEmpty()) {
+								break;
+							}
+							VoteResult itemVoteResult = new VoteResult();
+							VoteResultPK keyVoteResult = new VoteResultPK();
+
+							keyVoteResult.setSgId(item.get("sgId").toString());
+							keyVoteResult.setSgTypeCode(item.get("sgTypecode").toString());
+							keyVoteResult.setSggName(item.getString("sggName"));
+							keyVoteResult.setHuboOrder(seq);
+							itemVoteResult.setKey(keyVoteResult);
+							itemVoteResult.setSdName(item.getString("sdName"));
+							itemVoteResult.setWiwName(item.getString("wiwName"));
+							itemVoteResult.setSunsu(item.getBigDecimal("sunsu"));
+							itemVoteResult.setTusu(item.getBigDecimal("tusu"));
+							itemVoteResult.setYutusu(item.getBigDecimal("yutusu"));
+							itemVoteResult.setMutusu(item.getBigDecimal("mutusu"));
+							itemVoteResult.setGigwonsu(item.getBigDecimal("gigwonsu"));
+							itemVoteResult.setJd(item.getString("jd".concat(padLeft(seq, 2))));
+							itemVoteResult.setHbj(item.getString("hbj".concat(padLeft(seq, 2))));
+							itemVoteResult.setDugsu(item.getBigDecimal("dugsu".concat(padLeft(seq, 2))));
+							itemVoteResult.setCrOrder(item.getInt("crOrder"));
+							
+							listVoteResult.add(itemVoteResult);
+						}
+
+					}
+				} else
+				if (tmpJson.get("item") instanceof JSONObject) {
+					JSONObject item = tmpJson.getJSONObject("item");
+					VoteResult itemVoteResult = new VoteResult();
+					VoteResultPK keyVoteResult = new VoteResultPK();
+
+					keyVoteResult.setSgId(item.get("sgId").toString());
+					keyVoteResult.setSgTypeCode(item.get("sgTypecode").toString());
+					keyVoteResult.setSggName(item.getString("sggName"));
+					
+					// 후보자 seq 추출
+					for (int seq = 1; seq <= 30; seq++) {
+						String key = "jd".concat(padLeft(seq, 2));
+						if (!item.has(key)) {
+							break;
+						}
+						keyVoteResult.setHuboOrder(seq);
+						
+						itemVoteResult.setKey(keyVoteResult);
+						itemVoteResult.setSdName(item.getString("sdName"));
+						itemVoteResult.setWiwName(item.getString("wiwName"));
+						itemVoteResult.setSunsu(item.getBigDecimal("sunsu"));
+						itemVoteResult.setTusu(item.getBigDecimal("tusu"));
+						itemVoteResult.setMutusu(item.getBigDecimal("mutusu"));
+						itemVoteResult.setGigwonsu(item.getBigDecimal("gigwonsu"));
+						itemVoteResult.setJd(item.getString("jd".concat(padLeft(seq, 2))));
+						itemVoteResult.setHbj(item.getString("hbj".concat(padLeft(seq, 2))));
+						itemVoteResult.setDugsu(item.getBigDecimal("dugsu".concat(padLeft(seq, 2))));
+						itemVoteResult.setCrOrder(item.getInt("crOrder"));
+						
+						listVoteResult.add(itemVoteResult);
+					}
+				}
+
+				voteResultRepo.saveAll(listVoteResult);
+			//}
+			System.out.println("스케쥴러 End :: 개표 결과 조회");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String padLeft(int seq, int digit) {
+		return String.format("%" + digit + "s", seq).replace(' ', '0');
 	}
 }
