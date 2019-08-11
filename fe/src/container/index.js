1a6bcb96bb02af53588e74cbd6628360a353e8da
Jonghwi Lee <zziller03@gmail.com>
2019-08-11 19:42:32 +0900

    frontend 분리작업

--- fe/src/container/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Members } from "./Members";
-export { default as Discussion } from "./Discussion";
-export { default as Election } from "./Election";
--- fe/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { connectRouter } from "connected-react-router";
-import election from "./election";
-
-export default history =>
-  combineReducers({
-    router: connectRouter(history),
-    election
-  });
