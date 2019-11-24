dfde1a9fd801962fdad5b25f98e847e37e829db5
Jonghwi Lee <zziller03@gmail.com>
2019-11-25 02:32:14 +0900

    다운로드 페이지

--- src/main/java/dev/hwiveloper/naranim/NaranimApplication.java
+++ src/main/java/dev/hwiveloper/naranim/NaranimApplication.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.extern.slf4j.Slf4j;
@@ -36,6 +37,11 @@ public class NaranimApplication {
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name())
                 .allowCredentials(false)
                 .maxAge(3600);
+            }
+            
+            @Override
+            public void configureViewResolvers(ViewResolverRegistry registry) {
+            	registry.jsp("/WEB-INF/views/", ".jsp");
             }
         };
     }
---
+++ src/main/webapp/WEB-INF/views/download.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Insert title here</title>
+  </head>
+  <body>
+    <h1>APP DOWNLOAD</h1>
+    <a href="https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40hwiveloper/naranim-app-93ea1bad126f43bbae5ed8554b50fa62-signed.apk">Android</a>
+    <a href="#">iOS</a>
+  </body>
+</html>
