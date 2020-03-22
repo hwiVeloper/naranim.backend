acc64da51d95039e578daeb4ee3238cf82569cdd
Jonghwi Lee <zziller03@gmail.com>
2020-03-22 15:29:00 +0900

    빌드오류 수정

--- src/main/java/dev/hwiveloper/naranim/repository/RegionRepository.java
+++ src/main/java/dev/hwiveloper/naranim/repository/RegionRepository.java
@@ -1,14 +1,9 @@
 package dev.hwiveloper.naranim.repository;
 
-import java.util.List;
-
-import javax.annotation.Nullable;
-
 import org.springframework.data.repository.CrudRepository;
 
 import dev.hwiveloper.naranim.domain.common.Region;
 import dev.hwiveloper.naranim.domain.common.RegionPK;
 
 public interface RegionRepository extends CrudRepository<Region, RegionPK> {
-	List<Region> findAllByKey(@Nullable RegionPK key);
 }
